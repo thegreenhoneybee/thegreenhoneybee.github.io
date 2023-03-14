b4f4e8eb4c4ecb5f27d115374ee086d0c61332d2
Simon Windon <thegreenhoneybee@gmail.com>
2023-03-15 02:36:30 +1100

    Loading Elements

--- periodic_table/sketch.js
+++ periodic_table/sketch.js
@@ -1,7 +1,22 @@
-window.addEventListener('load', () => {
-    getElements()
-})
-
+getElements()
+var elementData
 async function getElements() {
-    
+    elementData = await fetch('element_data.json', {cache: 'no-cache'}).then(response => response.json())
+    console.log(elementData)
+    for (let data of elementData.elements) {
+        let element = elementTemplate.content.querySelector('.element').cloneNode(true)
+        element.innerHTML = data.Name
+        if (data.AtomicNumber >= 57 && data.AtomicNumber <= 71) {
+            // element.style.left = `${((data.AtomicNumber - 55) / 18) * 100}%`
+            // element.style.top = `${90}%`
+        } else if (data.AtomicNumber >= 89 && data.AtomicNumber <= 103) {
+            // element.style.left = `${((data.AtomicNumber - 87) / 18) * 100}%`
+            // element.style.top = `${100}%`
+        } else {
+            element.style.left = `${(data.Group - 1) * 5.5 + 0.75}%`
+            element.style.top = `${(data.Period - 1) * 11}%`
+        }
+        
+        container.appendChild(element)
+    }
 }
